507259456df83fdb5dd30d5719fc2e2fdfded208
Dennis Sauve <dengsauve@yahoo.com>
2020-10-28 20:32:10 -0700

    Created navbar

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <Navigation/>
+
+
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Navigation from "@/components/Navigation";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {Navigation}
 }
 </script>
 
@@ -23,6 +25,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
