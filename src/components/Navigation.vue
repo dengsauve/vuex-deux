ed410eba2c31628941e90b8dc3a5817a0d14124a
Dennis Sauve <dengsauve@yahoo.com>
2020-11-06 20:29:50 -0800

    Update Navigation.vue
    Ability to add a fresh list

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -5,7 +5,10 @@
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
-    <button class="btn btn-outline-secondary" v-if="add">+ Add List</button>
+
+    <button class="btn btn-outline-secondary" v-if="add" @click="addList">
+      + Add List
+    </button>
   </nav>
 </template>
 <script>
@@ -15,6 +18,15 @@ export default {
       'title',
       'searchbar',
       'add',
-  ]
+  ],
+  methods: {
+    addList: function () {
+      this.$store.state.lists.push({
+        id: this.$store.state.lists.length,
+        name: 'New List',
+        tasks: [],
+      })
+    }
+  }
 }
 </script>
