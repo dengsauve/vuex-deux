0d3fc0ef3314f3aae3a53229a75498b6f6178c4a
Dennis Sauve <dengsauve@yahoo.com>
2020-11-16 19:19:38 -0800

    Update Navigation.vue
    Disabling search for the time being

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -2,8 +2,8 @@
   <nav class="navbar navbar-light bg-light">
     <a class="navbar-brand">{{title}}</a>
     <form class="form-inline" v-if="searchbar">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" disabled>
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit" disabled>Search</button>
     </form>
 
     <button class="btn btn-outline-secondary" v-if="add" @click="addList">
